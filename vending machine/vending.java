b119adfe0157ace5517cf97f512e742eafc4e5e5
ecdthegreat <ecdthegreat@gmail.com>
2020-12-23 16:03:09 -0500

    fixed misspellings

--- vending machine/vending.java
+++ vending machine/vending.java
@@ -3,10 +3,10 @@ public class vending {
     public static void main(String[] args){
         Scanner input = new  Scanner(System.in);
         
-        System.out.println("Press \n1_Cold drinks \n2_Snacks \n3_ cereal \n input: ");
+        System.out.println("Press \n1_Cold drinks \n2_Snacks \n3_cereal \n input: ");
         int userInput = input.nextInt();
         if(userInput == 1){
-            System.out.println("Press \n1_Cola (3$) \n2_Dr.Pepper(4$) \n3_ Pepsi (5$) \n input: ");
+            System.out.println("Press \n1_Cola (3$) \n2_Dr.Pepper(4$) \n3_Pepsi (5$) \n input: ");
             int userSubSelection = input.nextInt();
             if(userSubSelection == 1){
                 System.out.println("how many colas");
@@ -32,7 +32,7 @@ public class vending {
         }
         else if(userInput == 2){
             System.out.println("you selected Snacks");
-            System.out.println("Press \n1_bbq chips(4.25) \n2_salt and vinger chips(3.99) \n3_ cookies(1.99) \n input: ");
+            System.out.println("Press \n1_bbq chips(4.25) \n2_salt and vinger chips(3.99) \n3_cookies(1.99) \n input: ");
             int userSubSelection = input.nextInt();
             if(userSubSelection == 1){
                 System.out.println("how many bbq chips");
@@ -57,7 +57,7 @@ public class vending {
             }
         }
         else if(userInput == 3){
-            System.out.println("Press \n1_Cheerios(5.99) \n2_Captain Crunch(6.99) \n3_Fruit loops(2.99)  \n imput ::");
+            System.out.println("Press \n1_Cheerios(5.99) \n2_Captain Crunch(6.99) \n3_Fruit loops(2.99)  \n input:");
             int userSubSelection = input.nextInt();
             if(userSubSelection == 1){
                 System.out.println("how many Cheerios");
