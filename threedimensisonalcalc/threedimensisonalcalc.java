88db6d56fe2d9504d98aece7c7c4913393aa9a52
ecdthegreat <ecdthegreat@gmail.com>
2021-01-22 10:50:54 -0500

    Added user input

--- threedimensisonalcalc/threedimensisonalcalc.java
+++ threedimensisonalcalc/threedimensisonalcalc.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
 public class ThreeDimensisonalCalc{
     static void calc(double x, double y, double z, double x2, double y2, double z2){
+        
         double unSquaredAnswer;
         double Result;
         double x3 = Math.pow(x2 - x, 2);
@@ -8,16 +10,25 @@ public class ThreeDimensisonalCalc{
 
         unSquaredAnswer = x3 + y3 + z3;
         Result = Math.sqrt(unSquaredAnswer);
-        System.out.println(Result);
+        System.out.println("Your answer is " + Result);
 
     }
     public static void main(String[] args){
-        double x = 8d;
-        double y = 12d;
-        double z = 30d;
-        double x2 = 19d;
-        double y2 = 22d;
-        double z2 = 40d;
+        Scanner input = new Scanner(System.in);
+        System.out.println("type your first int: ");
+        double x = input.nextDouble();
+        System.out.println("type your second int: ");
+        double y = input.nextDouble();
+        System.out.println("type your third int: ");
+        double z = input.nextDouble();
+        System.out.println("type your second set of ints");
+        System.out.println("type your first int: ");
+        double x2 = input.nextDouble();
+        System.out.println("type your second int: ");
+        double y2 = input.nextDouble();
+        System.out.println("type your third int: ");
+        double z2 = input.nextDouble();
         calc(x, y, z, x2, y2, z2);
+        input.close();
     }
 }
