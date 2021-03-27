c79c475c95c85c27e84aa4f12c5ee54bf301792b
ecdthegreat <ecdthegreat@gmail.com>
2021-03-27 14:07:47 -0400

    Add files via upload
    added a new project

---
+++ pathfinder/src/Main.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Window m = new Window();
+
+        JFrame f = new JFrame("Pathfinder");
+
+        m.setSize(Window.windowX, Window.windowY);
+        f.add(m);
+        f.setSize(Window.windowX, Window.windowY);
+        f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);
+        //f.setLayout(null);
+        f.setVisible(true);
+    }
+}
